0d145d2b635ce8c9edc84ed570224a03e21adeda
Laurensius Adrian Wibowo <elaawers@gmail.com>
2021-01-02 15:30:14 +0700

    [law] backup

--- sorting: Bubble Sort/bubbleSorting.js
+++ sorting: Bubble Sort/bubbleSorting.js
@@ -24,3 +24,23 @@ function countSwaps(a) {
 
 }
 countSwaps(a)
+
+const bubble = (arr, counter) => {
+    console.log(counter)
+    const len = arr.length
+    for (let i = 0; i < len; i++) {
+        //  if arr[i] is > arr[i+1] swap them
+        if (arr[i] > arr[i + 1]) {
+            let [before, after] = arr.slice(i, i + 2)
+            arr[i] = after
+            arr[i + 1] = before
+            console.log(`[${after} ${before}] ->`, arr.join(' '))
+            return bubble(arr, counter + 1)
+        }
+        else if (i == len - 1) {
+            return counter
+        }
+    }
+}
+
+console.log(bubble([4, 9, 7, 5, 8, 9, 3], 0))
